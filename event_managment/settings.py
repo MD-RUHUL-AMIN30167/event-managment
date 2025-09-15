577f0d87ad94fcd6c9435dd7992319c6614595e3
MD RUHUL AMIN <mdruhulamin@email.com>
2025-09-15 16:15:58 +0700

    postql success

--- event_managment/settings.py
+++ event_managment/settings.py
@@ -92,23 +92,23 @@ WSGI_APPLICATION = 'event_managment.wsgi.application'
 # }
 
 # using postql 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='postgresql://task_managment_db_wquo_user:6xssyHDmvYJzdvXdDksoWgy9V8NgqI4m@dpg-d2lla1fdiees73c56fs0-a.oregon-postgres.render.com/task_managment_db_wquo',
-#         conn_max_age=600
-#     )
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_managment',  
-        'USER': 'postgres',         # Default PostgreSQL user
-        'PASSWORD': '301678',       # তোমার password
-        'HOST': 'localhost',        # যদি local system হয়
-        'PORT': '5432',             # PostgreSQL default port
-    }
+    'default': dj_database_url.config(
+        default='postgresql://task_managment_db_wquo_user:6xssyHDmvYJzdvXdDksoWgy9V8NgqI4m@dpg-d2lla1fdiees73c56fs0-a.oregon-postgres.render.com/task_managment_db_wquo',
+        conn_max_age=600
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event_managment',  
+#         'USER': 'postgres',         
+#         'PASSWORD': '301678',       
+#         'HOST': 'localhost',        
+#         'PORT': '5432',             
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
